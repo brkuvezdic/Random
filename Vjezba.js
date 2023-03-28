62a91b2a24122ca2b788222109b0a02feb55f590
brkuvezdic <branimirkuvezdic@gmail.com>
2023-03-28 23:30:16 +0200

    Prvi commit

---
+++ Vjezba.js
@@ -0,0 +1,9 @@
+function FizzBuzz() {
+  console.log("Pocetak FizzBuzz funkcije");
+
+  for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) console.log(i);
+    else console.log("Nista");
+  }
+}
+FizzBuzz();
